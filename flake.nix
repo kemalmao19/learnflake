a672c4bed7e12c0a501c53e99d059fd27684ae97
kemalmao19 <sebelumtidur14@gmail.com>
2024-02-18 21:40:58 +0700

    use let and add devshells

--- flake.nix
+++ flake.nix
@@ -3,9 +3,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  outputs = inputs: {
-    devShells.x86_64-darwin.default = inputs.nixpkgs.legacyPackages.x86_64-darwin.mkShell {
-      buildInputs = [ inputs.nixpkgs.legacyPackages.x86_64-darwin.python3 ];
+  outputs = inputs:
+    let pkgs = inputs.nixpkgs.legacyPackages.x86_64-darwin;
+    in
+    {
+      devShells.x86_64-darwin = {
+        default = pkgs.mkShell {
+          buildInputs = [ ];
+        };
+        # python 
+        python = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python3
+          ];
+        };
+      };
     };
-  };
 }
